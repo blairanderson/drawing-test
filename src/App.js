29f56d74c702d31b4a92aed1e338fef1d04a38b2
Blair Anderson <blair81@gmail.com>
2022-05-30 12:10:53 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import CanvasDraw from "react-canvas-draw";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="text-center">
+        <h1>Signature App</h1>
       </header>
+      <div className="container">
+        <div className="row">
+          <div className="col-4 mx-auto">
+            <CanvasDraw />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
